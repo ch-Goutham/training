aec205ff7fb657bea21621c4a17db03355a07f36
ch-Goutham <gouthamyadav202@gmail.com>
2025-01-07 18:45:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+// console.log("start");
+
+// // global scope
+// var a =10
+// console.log(a);
+// // local scope
+// let b =20
+// console.log(b);
+
+// const c =30
+// console.log(c);
+
+// console.log("end");
+
+// // block scope
+// {
+//   var m =100
+//   console.log(m); 
+
+//   let n = 200
+//   console.log(n);
+
+//   const o = 300
+//   console.log(0);
+  
+  
+  
+// }
+
+// let _name1="abc"
+// document.write(_name1)
+
+// // reassiging variables
+// // var keyword
+
+// var a=1
+// a=5
+// console.log(a+a)
+// document.write(a+a);
+
+// // Let Keyword
+// let d=3;
+// d=7
+// console.log(d)
+
+// var a='def'
+// {
+//   var a ='fed'
+//   console.log(a)
+// }
+// console.log(a)
+
+// let e ="klm"
+// {
+//   let e = "mln"
+//   console.log(e)
+// }
+// console.log(e)
+
+// const f ="klm"
+// {
+//   const f = "mln"
+//   console.log(f)
+// }
+// console.log(f)
+
+// let g =1
+// console.log(`hi+ $(g+g)`)
+// document.write(`hi+ $(g+g)`)
+// console.log('hi+g+g')
+// console.log("hi+ g+g")
+
+let a = "sai" 
+let b= "kumar"
+let age = 30
+console.log(`Iam ${a+b},my age is ${age} is years old`)
+document.write(`Iam ${a+' '+b}, my age is ${age} years old`)
